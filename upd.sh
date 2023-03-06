b13ddb1be61e9ea8d8258a1e8141a748c0a9f902
غياث-Gheiath <ajamghiath@gmail.com>
2023-03-06 07:19:04 +0200

    ADDED shell best practices (tmp.sh)

--- upd.sh
+++ upd.sh
@@ -1,44 +1,67 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# PATHS
-# WSL:
-REPO=$HOME"/Desktop/myrepos/configs"
-BASH=$HOME"/.bashrc"
-TMUX=$HOME"/.tmux.conf.local"
-GIT=$HOME"/.config/git/"
-BAT=$HOME"/.config/bat/"
-NVIM=$HOME"/.config/nvim/"
-LUNARVIM=$HOME"/.config/lvim/"
-# Windows:
-WSL=$WINHOME"/.wslconfig"
-scoop=$WINHOME"/scoop"
-MPV=$scoop"/persist/mpv/portable_config/"
-ALACRITTY=$scoop"/persist/alacritty/alacritty.yml"
-WSLTTY=$scoop"/persist/wsltty/config/config"
-WEZTERM=$scoop"/persist/wezterm/wezterm.lua"
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then
+  set -o xtrace
+fi
 
-# 1D array, use spaces for split
-declare -a configs
-configs=(
-  # "$BASH bash_wsl/.bashrc"
-  # "$TMUX tmux_wsl/.tmux.conf.local"
-  # "$GIT git/"
-  # "$BAT bat_wsl"
-  # "$NVIM nvim_wsl/"
-  # "$LUNARVIM lvim_wsl/"
-  "$WSL wsl_distros_global/.wslconfig"
-  "$MPV mpv_win/"
-  "$ALACRITTY alacritty_win/alacritty.yml"
-  "$WSLTTY wsltty/config"
-  "$WEZTERM wezterm/wezterm.lua"
-)
+if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
+  echo 'Usage: ./upd.sh
 
-for pair in "${configs[@]}"; do
-  ar=($pair)
-  path=${ar[0]}
-  name=$REPO/${ar[1]}
-  printf "Copying $path To:\t $name \n"
-  # printf "Command: rsync -a $path $name \n\n"
-  # rsync -avh --progress $path $name
-  # printf "\n\n"
-done
+  this script rsyncs dotfiles from windows into dotfiles repo inside WSL
+'
+  exit
+fi
+
+cd "$(dirname "$0")"
+
+main() {
+  # PATHS
+  # WSL:
+  REPO=$HOME"/Desktop/myrepos/configs"
+  BASH=$HOME"/.bashrc"
+  TMUX=$HOME"/.tmux.conf.local"
+  GIT=$HOME"/.config/git/"
+  BAT=$HOME"/.config/bat/"
+  NVIM=$HOME"/.config/nvim/"
+  LUNARVIM=$HOME"/.config/lvim/"
+  ZATHURA=$HOME"/.config/zathura/"
+  # Windows:
+  WSL=$WINHOME"/.wslconfig"
+  scoop=$WINHOME"/scoop"
+  MPV=$scoop"/persist/mpv/portable_config/"
+  ALACRITTY=$scoop"/persist/alacritty/alacritty.yml"
+  WSLTTY=$scoop"/persist/wsltty/config/config"
+  WEZTERM=$scoop"/persist/wezterm/wezterm.lua"
+
+  # 1D array, use spaces for split
+  declare -a configs
+  configs=(
+    # "$BASH bash_wsl/.bashrc"
+    # "$TMUX tmux_wsl/.tmux.conf.local"
+    # "$GIT git/"
+    # "$BAT bat_wsl"
+    # "$NVIM nvim_wsl/"
+    # "$LUNARVIM lvim_wsl/"
+    # "$ZATHURA zathura_wsl/"
+    "$WSL wsl_distros_global/.wslconfig"
+    "$MPV mpv_win/"
+    "$ALACRITTY alacritty_win/alacritty.yml"
+    "$WSLTTY wsltty/config"
+    "$WEZTERM wezterm/wezterm.lua"
+  )
+
+  for pair in "${configs[@]}"; do
+    ar=($pair)
+    path=${ar[0]}
+    name=$REPO/${ar[1]}
+    printf "Copying $path To:\t $name \n"
+    printf "Command: rsync -a $path $name \n\n"
+    # rsync -avh --progress $path $name
+    printf "\n\n"
+  done
+}
+
+main "$@"
